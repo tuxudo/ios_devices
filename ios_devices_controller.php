f2ecdf27824f9939c474594b7bb1220c989a004f
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:34:51 -0400

    Update ios_devices_controller.php

--- ios_devices_controller.php
+++ ios_devices_controller.php
@@ -33,6 +33,9 @@ class Ios_devices_controller extends Module_controller
     **/
     public function get_tab_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
 
         if (! $this->authorized()) {
